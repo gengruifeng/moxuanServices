152b64424ea42211a9089b4280aa0eda42e32683
gengruifeng <gengruifeng@.com>
2018-10-17 17:54:34 +0800

    删除 moxuan ENv

--- MoXuan/Env/env.php
+++
@@ -1,3 +0,0 @@
-<?php
-//这个文件不要加到git版本库中
-define('APP_ENV', 'sample');
--- MoXuan/Env/sample.env.php
+++
@@ -1,7 +0,0 @@
-<?php
-//这个文件不要加到git版本库中
-//项目中的env配置文件只允许提交production,testing,demo三种,
-//但这三个文件在项目中也可以不存在
-
-defined('DEFAULT_REDIS_DB') || define('DEFAULT_REDIS_DB', '11');
-defined('DEFAULT_REDIS_PORT') || define('DEFAULT_REDIS_PORT', '6379');
